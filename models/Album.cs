dec79c9bd07b9ee412f15ab06c23b028a997919c
gui <augusto.g@outlook.com.br>
2024-07-07 17:40:03 -0300

    criacao models

---
+++ models/Album.cs
@@ -0,0 +1,12 @@
+namespace APIAlbumList.Models;
+
+public class APIAlbumList
+{
+    public int Id { get; set;}
+
+    public string ArtistName { get; set;}
+    public string Name { get; set;}
+
+    public DateOnly Release { get; set;}
+
+}
