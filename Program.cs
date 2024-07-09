b4445d4503dbdbcc448c5c5348270d9b99117b4b
gui <augusto.g@outlook.com.br>
2024-07-09 20:26:52 -0300

    add swagger and sql connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
+using APIAlbumList.Models;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Albums") ?? "Data Source=Albums.db";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<AlbumDb>(connectionString);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "AlbumList API",
+        Description = "Making the Pizzas you live",
+        Version = "v1"});
+});
 
 var app = builder.Build();
 
@@ -13,7 +29,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlbumList API V1");
+    });
 }
 
 app.UseHttpsRedirection();
--- models/Album.cs
+++ models/Album.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace APIAlbumList.Models;
 
 public class Album
 {
     public int Id { get; set;}
-
     public string? ArtistName { get; set;}
     public string? Name { get; set;}
-
     public int YearRelease { get; set;}
+}
 
+class AlbumDb : DbContext
+{
+    public AlbumDb(DbContextOptions<AlbumDb> options) : base(options) { }
+    public DbSet<Album> Albums { get; set;}
 }
